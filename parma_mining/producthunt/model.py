a73622923d3f8bb4b534e3cf74bdf63cb02ee020
Constantin Weberpals <constantin.weberpals@tum.de>
2024-01-02 22:29:44 +0100

    refactoring

--- parma_mining/producthunt/model.py
+++ parma_mining/producthunt/model.py
@@ -9,16 +9,33 @@ class CompaniesRequest(BaseModel):
     companies: dict[str, dict[str, list[str]]]
 
 
-class ResponseModel(BaseModel):
-    """Response model for ProductHunt data."""
-
-    source_name: str
-    company_id: str
-    raw_data: dict
-
-
 class DiscoveryModel(BaseModel):
     """Discovery model for ProductHunt data."""
 
     name: str | None
     url: str | None
+
+
+class Review(BaseModel):
+    """Review model."""
+
+    text: str | None
+    date: str | None
+
+
+class ProductInfo(BaseModel):
+    """Product model with data scraped from ProductHunt."""
+
+    name: str | None
+    overall_rating: float | None
+    review_count: int
+    followers: int
+    reviews: list[Review]
+
+
+class ResponseModel(BaseModel):
+    """Response model for ProductHunt data."""
+
+    source_name: str
+    company_id: str
+    raw_data: ProductInfo
--- parma_mining/producthunt/scraper.py
+++ parma_mining/producthunt/scraper.py
@@ -1,9 +1,12 @@
 """Product Hunt client module."""
+import logging
 import re
 from datetime import datetime
 
 import httpx
 from bs4 import BeautifulSoup
+
+from parma_mining.producthunt.model import ProductInfo
 
 
 class ProductHuntScraper:
@@ -12,62 +15,55 @@ class ProductHuntScraper:
     def __init__(self):
         """Initialize the Product Hunt client."""
         self.base_url = "https://www.producthunt.com/"
+        self.logger = logging.getLogger(__name__)
 
     def query_company_top_products(self, company_name: str) -> list:
         """Get links of products by company name."""
-        search_url = "https://www.producthunt.com/search"
+        search_url = self.base_url + "search"
         params = {"q": company_name}
 
-        with httpx.Client() as client:
-            response = client.get(search_url, params=params)
+        try:
+            with httpx.Client() as client:
+                response = client.get(search_url, params=params)
 
-        soup = BeautifulSoup(response.content, "html.parser")
+            soup = BeautifulSoup(response.content, "html.parser")
 
-        products = []
-        product_cut_off = 5
-        # Find all product name divs
-        product_name_divs = soup.find_all("div", {"data-test": "product-item-name"})
+            products = []
+            product_cut_off = 5
+            product_name_divs = soup.find_all("div", {"data-test": "product-item-name"})
 
-        for div in product_name_divs:
-            # Extract product name
-            product_name = div.get_text(strip=True)
+            for div in product_name_divs:
+                product_name = div.get_text(strip=True)
+                product_link = div.find_previous("a", href=True)
 
-            # Attempt to find the corresponding 'a' tag with product URL
-            product_link = div.find_previous("a", href=True)
-            if product_link and product_link["href"].startswith("/products/"):
-                full_url = "https://www.producthunt.com" + product_link["href"]
-                products.append({"name": product_name, "url": full_url})
+                if product_link and product_link["href"].startswith("/products/"):
+                    full_url = self.base_url.rstrip("/") + product_link["href"]
+                    products.append({"name": product_name, "url": full_url})
 
-                if len(products) >= product_cut_off:
-                    break
+                    if len(products) >= product_cut_off:
+                        break
 
-        return products
+            return products
+        except Exception as e:
+            self.logger.error(f"Failed to query company products: {e}")
+            return []
 
-    def scrape_product_page(self, url):
-        """Scrape product data based on the given url."""
-        with httpx.Client() as client:
-            response = client.get(url)
-            review_page_response = client.get(url + "/reviews?order=LATEST")
-
-        soup = BeautifulSoup(response.content, "html.parser")
-        review_soup = BeautifulSoup(review_page_response.content, "html.parser")
-
-        # Extract overall review rating from the review page
-        overall_rating_div = review_soup.find(
-            "div", class_="styles_reviewPositive__JY_9N"
-        )
-        overall_rating = (
+    def _extract_overall_rating(self, soup: BeautifulSoup) -> float | None:
+        overall_rating_div = soup.find("div", class_="styles_reviewPositive__JY_9N")
+        rating = (
             overall_rating_div.get_text(strip=True).split("/")[0]
             if overall_rating_div
             else None
         )
+        return float(rating)
 
-        # Extract the product name
-        product_name = soup.find("h1", class_="color-darker-grey").get_text(strip=True)
+    def _extract_product_name(self, soup: BeautifulSoup) -> str | None:
+        product_name_div = soup.find("h1", class_="color-darker-grey")
+        return product_name_div.get_text(strip=True) if product_name_div else None
 
-        # Extract the latest reviews
+    def _extract_reviews(self, soup: BeautifulSoup) -> list:
         reviews = []
-        for review_div in review_soup.find_all(
+        for review_div in soup.find_all(
             "div",
             class_="flex direction-column",
             id=lambda x: x and x.startswith("review-"),
@@ -76,8 +72,6 @@ class ProductHuntScraper:
             review_text = (
                 review_text_div.get_text(strip=True) if review_text_div else None
             )
-
-            # Extract the date of the review
             time_tag = review_div.find("time")
             review_date = (
                 datetime.fromisoformat(time_tag["datetime"]).strftime(
@@ -86,46 +80,46 @@ class ProductHuntScraper:
                 if time_tag
                 else None
             )
-
             reviews.append({"text": review_text, "date": review_date})
+        return reviews
 
-        # Extract the number of reviews
-        review_count_anchor = soup.find(
-            "a", href=lambda href: href and "reviews" in href
-        )
-        review_count_text = (
-            review_count_anchor.get_text(strip=True) if review_count_anchor else None
-        )
+    def _extract_followers(self, soup: BeautifulSoup) -> int:
+        followers_div = soup.find("div", class_="styles_count___6_8F")
+        if followers_div:
+            followers_text = followers_div.get_text(strip=True)
+            print(followers_text)
+            followers_match = re.search(r"(\d+(?:\.\d+)?)\s?[Kk]?", followers_text)
+            print(followers_match)
+            if followers_match:
+                followers_str = followers_match.group(1)
+                followers_count = float(followers_str)
+                print(followers_count)
 
-        # Use regular expression to find digits in the string
-        review_count_matches = (
-            re.findall(r"\d+", review_count_text) if review_count_text else []
-        )
-        review_count = int(review_count_matches[0]) if review_count_matches else 0
+                if "K" in followers_text or "k" in followers_text:
+                    followers_count *= 1000  # Convert thousands to actual number
 
-        # Extract followers
-        followers_div = soup.find("div", string=re.compile(r"\bfollowers\b", re.I))
-        followers_text = followers_div.get_text(strip=True) if followers_div else None
+                return int(followers_count)
 
-        followers = 0  # Default value in case followers count is not found
-        if followers_text:
-            followers_match = re.search(r"\b(\d+(?:\.\d+)?[Kk]?)\b", followers_text)
-            followers_str = followers_match.group(1) if followers_match else "0"
+        return 0
 
-            # Check if the followers count is in thousands and convert accordingly
-            if "K" in followers_str or "k" in followers_str:
-                followers = int(
-                    float(followers_str.replace("K", "").replace("k", "")) * 1000
-                )
-            else:
-                followers = int(followers_str)
+    def scrape_product_page(self, url: str) -> ProductInfo:
+        """Get Product data with link of product page."""
+        try:
+            with httpx.Client() as client:
+                response = client.get(url)
+                review_page_response = client.get(url + "/reviews?order=LATEST")
 
-        product_info = {
-            "name": product_name,
-            "overall_rating": overall_rating,
-            "review_count": review_count,
-            "followers": followers,
-            "reviews": reviews,
-        }
+            soup = BeautifulSoup(response.content, "html.parser")
+            review_soup = BeautifulSoup(review_page_response.content, "html.parser")
 
-        return product_info
+            product_info_data = {
+                "name": self._extract_product_name(soup),
+                "overall_rating": self._extract_overall_rating(review_soup),
+                "review_count": len(self._extract_reviews(review_soup)),
+                "followers": self._extract_followers(soup),
+                "reviews": self._extract_reviews(review_soup),
+            }
+            return ProductInfo(**product_info_data)
+        except Exception as e:
+            self.logger.error(f"Failed to scrape product page: {e}")
+            return ProductInfo()
