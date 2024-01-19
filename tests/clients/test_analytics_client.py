3a3159447870bedcca89b83d879b31696668f2a3
Ege Kocabas <kocabas.ege@tum.de>
2024-01-19 13:10:03 +0100

    update token

--- tests/clients/test_analytics_client.py
+++ tests/clients/test_analytics_client.py
@@ -7,7 +7,10 @@ from parma_mining.mining_common.const import HTTP_200, HTTP_500
 from parma_mining.producthunt.analytics_client import AnalyticsClient
 from parma_mining.producthunt.model import ProductInfo, ResponseModel, Review
 
-TOKEN = "mocked_token"
+
+@pytest.fixture
+def token():
+    return "mocked_token"
 
 
 @pytest.fixture
@@ -41,35 +44,35 @@ def mock_response_model(mock_organization_model):
 
 
 @patch("httpx.post")
-def test_send_post_request_success(mock_post, analytics_client):
+def test_send_post_request_success(mock_post, analytics_client, token):
     mock_post.return_value = httpx.Response(HTTP_200, json={"key": "value"})
     response = analytics_client.send_post_request(
-        TOKEN, "http://example.com", {"data": "test"}
+        token, "http://example.com", {"data": "test"}
     )
     assert response == {"key": "value"}
 
 
 @patch("httpx.post")
-def test_send_post_request_failure(mock_post, analytics_client):
+def test_send_post_request_failure(mock_post, analytics_client, token):
     mock_post.return_value = httpx.Response(HTTP_500, text="Internal Server Error")
     with pytest.raises(Exception) as exc_info:
         analytics_client.send_post_request(
-            TOKEN, "http://example.com", {"data": "test"}
+            token, "http://example.com", {"data": "test"}
         )
     assert "API request failed" in str(exc_info.value)
 
 
 @patch("httpx.post")
-def test_register_measurements(mock_post, analytics_client):
+def test_register_measurements(mock_post, analytics_client, token):
     mock_post.return_value = httpx.Response(HTTP_200, json={"id": "123"})
     mapping = {"Mappings": [{"DataType": "int", "MeasurementName": "test_metric"}]}
-    result, updated_mapping = analytics_client.register_measurements(TOKEN, mapping)
+    result, updated_mapping = analytics_client.register_measurements(token, mapping)
     assert "source_measurement_id" in updated_mapping["Mappings"][0]
     assert result[0]["source_measurement_id"] == "123"
 
 
 @patch("httpx.post")
-def test_feed_raw_data(mock_post, analytics_client, mock_response_model):
+def test_feed_raw_data(mock_post, analytics_client, mock_response_model, token):
     mock_post.return_value = httpx.Response(HTTP_200, json={"result": "success"})
-    result = analytics_client.feed_raw_data(TOKEN, mock_response_model)
+    result = analytics_client.feed_raw_data(token, mock_response_model)
     assert result == {"result": "success"}
